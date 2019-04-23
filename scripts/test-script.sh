04c125671b8646e6684062150696c98b93bd3f65
AnthonyAmanse <ghieamanse@gmail.com>
2019-04-23 13:28:46 -0700

    test
    Signed-off-by: AnthonyAmanse <ghieamanse@gmail.com>

--- scripts/test-script.sh
+++ scripts/test-script.sh
@@ -3,4 +3,4 @@
 cat "${HOME}"/.wskprops
 
 wsk -i list
-wsk -i action invoke /whisk.system/utils/echo --param test test --result
+wsk -i action invoke "/whisk.system/utils/echo" --param test test --result
